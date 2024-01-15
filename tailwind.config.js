cd202dcd2b7a9e023b277ae86f44d1ec6352e69a
Gian Mateus <gian.m.silver@gmail.com>
2024-01-15 19:57:16 -0300

    add coffee project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'hand-and-coffee': "url('assets/close-up-hands-barista-make-latte-coffee-art-paint.jpg')"
+      // }
+    },
+  },
+  plugins: [],
+}
+
